b585dab882211219ec09c5c3e3b4a6f675c92ed7
phanvongocphu <155410531+phanvongocphu@users.noreply.github.com>
2024-11-02 20:43:04 +0700

    Push

---
+++ FStore.SalesRazorApp/Pages/ProductPage/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FStore.BusinessObject;
+
+namespace FStore.SalesRazorApp.Pages.ProductPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly FStore.BusinessObject.AppDbContext _context;
+
+        public DetailsModel(FStore.BusinessObject.AppDbContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
